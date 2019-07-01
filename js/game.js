2e347e35d3cc5618471cd6e7f839695c8165c07b
fmamini <faam.amini@gmail.com>
2019-07-02 00:14:27 +0430

    add startGame function

--- js/game.js
+++ js/game.js
@@ -11,7 +11,7 @@ imgBlack = '<img src=' + 'https://raw.githubusercontent.com/pukie/othello-game/m
     for (var x = 1; x < 9; ++x) {
         table += '<tr>';     // ezafehkardan 8 satr
         for (var y = 1; y < 9; ++y) {
-            table += '<td class=othelloSquare id=' + x + y + '>' + '</td>';     // 8 sotun baraye har satr
+            table += '<td class=othelloSquare id ='+ x + y + '>' +  '</td>';     // 8 sotun baraye har satr
         }
     }
     table += " </table>";
@@ -21,6 +21,46 @@ imgBlack = '<img src=' + 'https://raw.githubusercontent.com/pukie/othello-game/m
 $('.othelloSquare').click(function () {
 
     if (!$(this).hasClass('black') && !$(this).hasClass('white')) {
-
+            // add the code
     }
+});
+
+
+function startGame() {
+    typeTd = 'black';
+    $('.pointsOfPlayerOne').removeClass('animateAreaScore');
+    $('.pointsOfPlayerTwo').removeClass('animateAreaScore');
+    $('.pointsOfPlayerOne').addClass('animateAreaScore');
+
+    $('.othelloSquare').removeClass('black white').html('');
+
+    pointStart1 = 20;
+    pointStart2 = 20;
+
+    $('.areaPoints1').html(pointStart1);
+    $('.areaPoints2').html(pointStart2);
+
+    $('#player1').html('player 1' + '<img src=' + 'https://raw.githubusercontent.com/pukie/othello-game/master/images/white.png' + ' class="playerIcon">');
+    $('#player2').html('player 2' + '<img src=' + 'https://raw.githubusercontent.com/pukie/othello-game/master/images/black.png' + ' class="playerIcon">');
+
+    $('.exchangeRateTable').css('visibility', 'visible');
+    $('.pointsOfPlayerOne').css('visibility', 'visible');
+    $('.pointsOfPlayerTwo').css('visibility', 'visible');
+
+    $('#44').html(imgBlack).addClass('black');
+    $('#54').html(imgBlack).addClass('black');
+    $('#45').html(imgWhite).addClass('white');
+    $('#55').html(imgWhite).addClass('white');
+
+}
+
+$("#startOthelloButton").click(function () {
+    document.getElementById("startOthelloButton").style.display = "none";
+
+    document.getElementById("resetOthelloButton").style.display = "inline";
+    startGame();
+});
+
+$('#resetOthelloButton').click(function () {
+    startGame();
 });
